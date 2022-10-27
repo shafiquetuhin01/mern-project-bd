7185099f630908f75d4588b98d56830120f5c5ec
unknown <sfqtuhin04@gmail.com>
2022-10-27 22:21:55 +0600

    updating module 1.50 updated

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const {logger} = require('./middleware/logger')
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const errorHandler = require('./middleware/errorHandler')
+const { logger } = require("./middleware/logger");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
 
-app.use(logger)
-app.use(cookieParser())
-app.use(cors(corsOptions))
+console.log(process.env.NODE_ENV);
+connectDB();
+
+app.use(logger);
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
-console.log(process.env.NODE_ENV)
+app.use('/users', require('./routes/userRoutes'))
+console.log(process.env.NODE_ENV);
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -27,5 +34,13 @@ app.all("*", (req, res) => {
   }
 });
 
-app.use(errorHandler)
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(errorHandler);
+mongoose.connection.once("open", () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on('error', err =>{
+  console.log(err)
+  logEvents(`${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`,'mongoErrLog.log')
+})
