e1db78c10bab039523f7dff6405c2cf9ed4ca450
unknown <sfqtuhin04@gmail.com>
2022-10-28 22:23:54 +0600

    updating database

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.progamminghero.com',
-    'https://progamminghero.com'
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
 ]
+
+module.exports = allowedOrigins
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/usersController');
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
 
 router.route('/')
-.get(usersController.getAllUsers)
-.post(usersController.createNewUser)
-.patch(usersController.updateUser)
-.delete(usersController.deleteUser)
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
 
 module.exports = router
